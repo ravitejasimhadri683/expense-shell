e9a8f784ba784ce4728a9d8df25ae8c8c83b7026
ravitejasimhadri683 <ravitejasimhadri683@gmail.com>
2025-03-22 21:27:02 +0530

    auto push

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 echo "Installing nginx: "
 dnf install nginx -y  &>> /tmp/frontend.log
+if [ $? -eq 0 ]; then
+    echo "Success"
+    else
+        echo "Failed"
+fi
 echo "Enable nginx: "
 systemctl enable nginx &>> /tmp/frontend.log
+if [ $? -eq 0 ]; then
+    echo "Success"
+    else
+        echo "Failed"
+fi
 echo "starting nginx: "
 systemctl start nginx  &>> /tmp/frontend.log
+if [ $? -eq 0 ]; then
+    echo "Success"
+    else
+        echo "Failed"
+fi
 # rm -rf /usr/share/nginx/html/*  
 # curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
 # cd /usr/share/nginx/html 
