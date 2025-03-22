952c7edcb9b93089b0d46485f0e4c209ba3f5137
ravitejasimhadri683 <ravitejasimhadri683@gmail.com>
2025-03-22 21:33:24 +0530

    auto push

--- frontend.sh
+++ frontend.sh
@@ -13,17 +13,29 @@ if [ $? -eq 0 ]; then
     else
         echo -e "\e[31m Failed \e[0m"
 fi
-echo "starting nginx: "
+echo -n "starting nginx: "
 systemctl start nginx  &>> /tmp/frontend.log
 if [ $? -eq 0 ]; then
     echo -e "\e[32m Success \e[0m"
     else
         echo -e "\e[31m Failed \e[0m"
 fi
-# rm -rf /usr/share/nginx/html/*  
-# curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
-# cd /usr/share/nginx/html 
-# unzip /tmp/frontend.zip
+echo -n "Remove old content" 
+rm -rf /usr/share/nginx/html/*  
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m Success \e[0m"
+    else
+        echo -e "\e[31m Failed \e[0m"
+fi
+echo -n "Downloading frontend content:"
+curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip &>> /tmp/frontend.log
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m Success \e[0m"
+    else
+        echo -e "\e[31m Failed \e[0m"
+fi
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
 # vim /etc/nginx/default.d/expense.conf   ( empty the file if any and add the below content )
 
 # proxy_http_version 1.1;
@@ -35,5 +47,10 @@ fi
 # access_log off;
 # }
 
-# systemctl restart nginx 
+systemctl restart nginx 
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m Success \e[0m"
+    else
+        echo -e "\e[31m Failed \e[0m"
+fi
 # systemctl status nginx 
