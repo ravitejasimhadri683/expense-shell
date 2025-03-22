f455cf27a21aa3711df3f9bcd2ecbaf19ff17010
ravitejasimhadri683 <ravitejasimhadri683@gmail.com>
2025-03-22 21:43:15 +0530

    auto push

--- frontend.sh
+++ frontend.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
+component="frontend"
 echo -n "Installing nginx: "
-dnf install nginx -y  &>> /tmp/frontend.log
+dnf install nginx -y  &>> /tmp/$component.log
 if [ $? -eq 0 ]; then
     echo -e "\e[32m Success \e[0m"
     else
         echo -e "\e[31m Failed \e[0m"
 fi
 echo -n "Enable nginx: "
-systemctl enable nginx &>> /tmp/frontend.log
+systemctl enable nginx &>> /tmp/$component.log
 if [ $? -eq 0 ]; then
     echo -e "\e[32m Success \e[0m"
     else
         echo -e "\e[31m Failed \e[0m"
 fi
 echo -n "starting nginx: "
-systemctl start nginx  &>> /tmp/frontend.log
+systemctl start nginx  &>> /tmp/$component.log
 if [ $? -eq 0 ]; then
     echo -e "\e[32m Success \e[0m"
     else
         echo -e "\e[31m Failed \e[0m"
 fi
-echo -n "Remove old content" 
+echo -n "Remove old content from $component" 
 rm -rf /usr/share/nginx/html/*  
 if [ $? -eq 0 ]; then
     echo -e "\e[32m Success \e[0m"
     else
         echo -e "\e[31m Failed \e[0m"
 fi
-echo -n "Downloading frontend content:"
-curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip &>> /tmp/frontend.log
+echo -n "Downloading $component content:"
+curl -o /tmp/$component.zip https://expense-web-app.s3.amazonaws.com/frontend.zip &>> /tmp/$component.log
 if [ $? -eq 0 ]; then
     echo -e "\e[32m Success \e[0m"
     else
         echo -e "\e[31m Failed \e[0m"
 fi
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip &>> /tmp/frontend.log
-cp expense.conf /etc/nginx/default.d/expense.conf  &>> /tmp/frontend.log
+unzip /tmp/$component.zip &>> /tmp/$component.log
+cp expense.conf /etc/nginx/default.d/expense.conf  &>> /tmp/fron$component.log
 # vim /etc/nginx/default.d/expense.conf   ( empty the file if any and add the below content )
 
 echo -n "Resarting nginx: "
